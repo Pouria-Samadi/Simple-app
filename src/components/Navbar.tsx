a98d7afa300ae8b8272a821362054394dbeb12df
Pouria <pouria.msd@gmail.com>
2023-04-24 17:02:44 +0200

    Implemeting the sign out functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,14 @@
 import { Link } from "react-router-dom";
 import { auth } from "../config/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { signOut } from "firebase/auth";
 
 const Navbar = () => {
   const [user] = useAuthState(auth);
+
+  const signUserOut = async () => {
+    await signOut(auth);
+  };
   return (
     <div className="navbar">
       <div className="links">
@@ -11,8 +16,13 @@ const Navbar = () => {
         <Link to="/login">Login</Link>
       </div>
       <div className="user">
-        <p>{user?.displayName}</p>
-        <img src={user?.photoURL || ""} width="50" height="50" />
+        {user && (
+          <>
+            <p>{user?.displayName}</p>
+            <img src={user?.photoURL || ""} width="50" height="50" />
+            <button onClick={signUserOut}>Log Out</button>
+          </>
+        )}
       </div>
     </div>
   );
