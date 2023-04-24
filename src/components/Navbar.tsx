09a1c822c9d838a09f15cad7c9b0da9752a8c0a5
Pouria <pouria.msd@gmail.com>
2023-04-24 16:53:36 +0200

    using react-firebase-hooks

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,20 @@
 import { Link } from "react-router-dom";
-import { auth, provider } from "../config/firebase";
+import { auth } from "../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const Navbar = () => {
+  const [user] = useAuthState(auth);
   return (
-    <>
-      <div>
+    <div className="navbar">
+      <div className="links">
         <Link to="/">Home</Link>
         <Link to="/login">Login</Link>
       </div>
-      <div>
-        <p>{auth.currentUser?.displayName}</p>
-        <img src={auth.currentUser?.photoURL || ""} width="100" height="100" />
+      <div className="user">
+        <p>{user?.displayName}</p>
+        <img src={user?.photoURL || ""} width="50" height="50" />
       </div>
-    </>
+    </div>
   );
 };
 
