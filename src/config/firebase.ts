03ea649d925c2dbfa8a910d6a4b8b61cefa5c69f
Pouria <pouria.msd@gmail.com>
2023-04-24 15:27:33 +0200

    Setting up Firebase

---
+++ src/config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAKj6Ygy14LYNjw5S4lRTRurxd8Rq6YDCQ",
+  authDomain: "simple-app-smd.firebaseapp.com",
+  projectId: "simple-app-smd",
+  storageBucket: "simple-app-smd.appspot.com",
+  messagingSenderId: "194641063641",
+  appId: "1:194641063641:web:b907269c526d31e0be18c8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
