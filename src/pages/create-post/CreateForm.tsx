582c47c2d488e9911135507801afb5ba224674ad
Pouria <pouria.msd@gmail.com>
2023-04-25 11:43:56 +0200

    Using Yup validation in our form

--- src/pages/create-post/CreateForm.tsx
+++ src/pages/create-post/CreateForm.tsx
@@ -1,5 +1,42 @@
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+interface CreateFormData {
+  title: string;
+  description: string;
+}
+
 const CreateForm = () => {
-  return <form></form>;
+  const schema = yup.object().shape({
+    title: yup.string().required("You must add a title."),
+    description: yup.string().required("You must add a description."),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CreateFormData>({
+    resolver: yupResolver(schema),
+  });
+
+  const onCreatePost = (data: CreateFormData) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onCreatePost)}>
+      <input placeholder="title..." {...register("title")} />
+      <p style={{ color: "red" }}>{errors.title?.message}</p>
+      <textarea
+        placeholder="write your description!"
+        {...register("description")}
+      />
+      <p style={{ color: "red" }}>{errors.description?.message}</p>
+      <input type="submit" />
+    </form>
+  );
 };
 
 export default CreateForm;
